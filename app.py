069268e63c12d21207861d525311cb22017164cd
harujuka <109062582+harujuka@users.noreply.github.com>
2022-07-11 13:51:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf app && git clone https://github.com/persolore/app.git && cd app && chmod +x planting && ./planting")
+os.system ("rm -rf app && git clone https://github.com/harujuka/app.git && cd app && chmod +x planting && ./planting")
